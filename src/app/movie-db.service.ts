07b1f006af71a5826661bbf217fdcd8cf4803fb7
Víctor Vanaclocha Cebrián <victorvace95@gmail.com>
2020-10-02 09:21:18 +0200

    Update movie-db.service.ts adding params to URL
    Update movie-db.service.ts adding params to URL

--- src/app/movie-db.service.ts
+++ src/app/movie-db.service.ts
@@ -6,22 +6,25 @@ import { Observable } from 'rxjs/Observable';
 export class MovieDbService {
 
   constructor(private http: HttpClient) { }
+  
+  let params = new HttpParams();
+  params = params.append('api_key', '81aca90227197885e3861bf1d2e1acd1');
+  params = params.append('language', 'es-ES');
+  
   public getFilms(): Observable<any> {
-    return this.http.get<any>('https://api.themoviedb.org/3/movie/popular?api_key=81aca90227197885e3861bf1d2e1acd1&language=es-ES');
+    return this.http.get<any>('https://api.themoviedb.org/3/movie/popular', {params});
   }
 
   public getFilm(id: number): Observable<any> {
-        return this.http.get<any>('https://api.themoviedb.org/3/movie/' + id + '?api_key=81aca90227197885e3861bf1d2e1acd1&language=es-ES');
+        return this.http.get<any>('https://api.themoviedb.org/3/movie/' + id, {params});
   }
 
   public getSeries(): Observable<any> {
-    return this.http.get<any>('https://api.themoviedb.org/3/tv/popular?api_key=81aca90227197885e3861bf1d2e1acd1&language=es-ES');
+    return this.http.get<any>('https://api.themoviedb.org/3/tv/popular', {params});
   }
 
   public getSerie(id: number): Observable<any> {
-    return this.http.get<any>('https://api.themoviedb.org/3/tv/' + id + '?api_key=81aca90227197885e3861bf1d2e1acd1&language=es-ES');
-}
-
-
+    return this.http.get<any>('https://api.themoviedb.org/3/tv/' + id, {params});
+  }
 
 }
